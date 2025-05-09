2e419f581dc13920e2cdbe46d6fa3267579707bf
vera0140 <a100240140@my.uvm.edu.mx>
2025-05-09 16:32:56 -0600

    Delete mi_script.js

--- mi_script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const miBoton = document.getElementById('miBoton');
-
-    miBoton.addEventListener('click', function() {
-        alert('¡Hola! Este es un botón interactivo.');
-    });
-});
